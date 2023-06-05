e3da7e26185efa37d79f6a1847b16e8e0e549c9f
lionj <judin.malivert@poe.aelion.biz>
2023-06-06 01:30:19 +0200

    student changes and simple student

--- src/main/java/fr/aelion/streamer/controllers/StudentController.java
+++ src/main/java/fr/aelion/streamer/controllers/StudentController.java
@@ -1,15 +1,21 @@
 package fr.aelion.streamer.controllers;
 
+import fr.aelion.streamer.dto.AddStudentDto;
+import fr.aelion.streamer.dto.SimpleStudentDto;
+import fr.aelion.streamer.dto.SimpleStudentProjection;
 import fr.aelion.streamer.entities.Student;
 import fr.aelion.streamer.services.StudentService;
+import fr.aelion.streamer.services.exceptions.EmailAlreadyExistsException;
+import fr.aelion.streamer.services.exceptions.LoginAlreadyExistsException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
 @RestController
 @RequestMapping("api/v1/students") //http://127.0.0.1:8080/api/v1/students
@@ -18,9 +24,9 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
-@GetMapping
-@CrossOrigin
-    public List<Student> findAll(){
+    @GetMapping
+    @CrossOrigin
+    public List<Student> findAll() {
 
     /*List<Student> students = new ArrayList<>();
     Student student = new Student();
@@ -32,6 +38,81 @@ public class StudentController {
     student.setLogin("jlaubert");
     student.setPassword("jla");
     students.add(student);*/
-    return studentService.findAll();
-}
+        return studentService.findAll();
+    }
+
+    @GetMapping("{id}") // Get http://127.0.0.1:5000/api/v1/students/1
+    @CrossOrigin
+    public ResponseEntity<?> findOne(@PathVariable int id) {
+        try {
+            return ResponseEntity.ok(studentService.findOne(id));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("simple")
+    @CrossOrigin
+    public List<SimpleStudentProjection> findSimpleStudents() {
+        return studentService.fromProjection();
+    }
+
+    @CrossOrigin
+    @GetMapping("dto")
+    public List<SimpleStudentDto> simpleStudentDtos() {
+        return studentService.findSimpleStudents();
+    }
+
+    /**
+     * POST a new student
+     * uri : POST http://127.0.0.1:5000/api/v1/students
+     *
+     * @param student
+     * @return
+     */
+    @PostMapping
+    @CrossOrigin
+    public ResponseEntity<?> add(@Valid @RequestBody AddStudentDto student) {
+        try {
+            Student newStudent = studentService.add(student);
+            return ResponseEntity.created(null).body(newStudent);
+        } catch (EmailAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.reject());
+        } catch (LoginAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body((e.reject()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @PutMapping
+    @CrossOrigin
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<?> update(@RequestBody Student student) {
+        try {
+            studentService.update(student);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    @CrossOrigin
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<?> singleDelete(@PathVariable int id) {
+        try {
+            studentService.delete(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping()
+    @CrossOrigin
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> multipleDelete(@RequestBody Set<Integer> ids) {
+        return ResponseEntity.ok(studentService.multipleDelete(ids));
+    }
 }
--- src/main/java/fr/aelion/streamer/services/StudentService.java
+++ src/main/java/fr/aelion/streamer/services/StudentService.java
@@ -1,18 +1,104 @@
 package fr.aelion.streamer.services;
 
+import fr.aelion.streamer.dto.AddStudentDto;
+import fr.aelion.streamer.dto.SimpleStudentDto;
+import fr.aelion.streamer.dto.SimpleStudentProjection;
 import fr.aelion.streamer.entities.Student;
+import fr.aelion.streamer.repositories.StudentRepository;
+import fr.aelion.streamer.services.exceptions.EmailAlreadyExistsException;
+import fr.aelion.streamer.services.exceptions.LoginAlreadyExistsException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
     @Autowired
-    private JpaRepository repository;
+    private StudentRepository repository;
 
-    public List<Student> findAll(){
-        return repository.findAll();
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<Student> findAll() {
+        List<Student> students = repository.findAll();
+        return students;
+    }
+
+    public List<SimpleStudentDto> findSimpleStudents() {
+        return repository.findAll()
+                .stream()
+                .map(s -> {
+                    SimpleStudentDto dto = new SimpleStudentDto();
+                    dto.setId(s.getId());
+                    dto.setLastName(s.getLastName());
+                    dto.setFirstName(s.getFirstName());
+                    dto.setEmail(s.getEmail());
+                    return dto;
+                })
+                .collect(Collectors.toList());
+    }
+
+    public List<SimpleStudentProjection> fromProjection() {
+        return repository.getSimpleStudents();
+    }
+
+    public Student add(AddStudentDto student) throws Exception {
+
+        Student anyStudent = repository.findByEmail(student.getEmail());
+        if (anyStudent != null) {
+            throw new EmailAlreadyExistsException("Email " + student.getEmail() + " already exists");
+        }
+        anyStudent = repository.findByLogin(student.getLogin());
+        if (anyStudent != null) {
+            throw new LoginAlreadyExistsException("Login " + student.getLogin() + " already exists");
+        }
+        Student newStudent = modelMapper.map(student, Student.class);
+        newStudent = (Student) repository.save(newStudent);
+
+        return newStudent;
+    }
+
+    public void update(Student student) throws Exception {
+        try {
+            repository.save(student);
+        } catch (Exception e) {
+            throw new Exception("Something went wrong while updating Student");
+        }
+    }
+
+    public Student findOne(int id) {
+        return repository.findById(id)
+                .map(s -> s)
+                .orElseThrow();
+    }
+
+    public void delete(int id) {
+        try {
+            var student = this.findOne(id);
+            repository.delete(student);
+        } catch (NoSuchElementException e) {
+            throw e;
+        }
+    }
+
+    public Set<Integer> multipleDelete(Set<Integer> ids) {
+        var nonDeletedIds = new HashSet<Integer>();
+        ids.stream()
+                .forEach(i -> {
+                    try {
+                        repository.delete(this.findOne(i));
+                    } catch(NoSuchElementException e) {
+                        nonDeletedIds.add(i);
+                    } catch (Exception e) {
+                        nonDeletedIds.add(i);
+                    }
+                });
+        return nonDeletedIds;
     }
 }
